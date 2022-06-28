1009ee10c2fd5157d69a0a1e103dfec1865fee7b
Amr Essam <79043600+amressam93@users.noreply.github.com>
2022-06-28 13:37:31 +0200

    implement Leaderboard and event listeners

---
+++ app/Events/ScoreUpdated.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ScoreUpdated implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+
+
+    public $user;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+
+
+
+    public function broadcastOn()
+    {
+        return new Channel('leaderboard');
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'id' => $this->user->id,
+            'name' => $this->user->name,
+            'score' => $this->user->score
+        ];
+    }
+
+
+
+}
